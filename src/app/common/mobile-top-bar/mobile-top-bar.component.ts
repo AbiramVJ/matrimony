d761b98f26856a09f634991520dae7d1dd5a2d2d
abiram <abiram586@gmail.com>
2025-06-04 13:40:34 +0530

    mobile responsive

---
+++ src/app/common/mobile-top-bar/mobile-top-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mobile-top-bar',
+  imports: [],
+  templateUrl: './mobile-top-bar.component.html',
+  styleUrl: './mobile-top-bar.component.scss'
+})
+export class MobileTopBarComponent {
+
+}
